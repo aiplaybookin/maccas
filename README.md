9e60e207b944b7d569ad29f27206122b30b3b3fc
aiplaybookin <aiplaybook.in@gmail.com>
2023-07-14 13:17:08 +0000

    cifar training -1

--- README.md
+++ README.md
@@ -18,7 +18,20 @@ Say, huggingface instead of default from configs.
 ```maccas_train model=hf```
 
 
-## Use CIFAR10 dataset
+## Use CIFAR10 dataset and timm pretrained models
+
+Reference repo https://github.com/ashleve/lightning-hydra-template
 
 - Add cifa10_datamodule.py in maccas/data folder with class of data module.
-- Add cifar10.yaml in configs/data folder to instantiate the data model for training
+- Add cifar10.yaml in configs/data folder to instantiate the data model for training with previous step class references
+- Add timm_module.py in maccas/models
+- Add timm.yaml to configs folder with previous 1 step references
+
+- Add eval.py to maccas folder similar to train.py
+- Add eval.yaml to configs folder ( user must provide checkpoint path )
+
+maccas_train data=cifar10 model=timm
+
+maccas_eval --help
+
+pip install timm==0.9.2
---
+++ maccas/models/timm_module.py
@@ -0,0 +1,167 @@
+from typing import Any, List
+
+import torch
+from lightning import LightningModule
+from torchmetrics import MaxMetric, MeanMetric
+from torchmetrics.classification.accuracy import Accuracy
+from torchvision import transforms as T
+
+
+class TIMMLitModule(LightningModule):
+    """Example of LightningModule for CIFAR10 classification.
+    A LightningModule organizes your PyTorch code into 6 sections:
+        - Computations (init).
+        - Train loop (training_step)
+        - Validation loop (validation_step)
+        - Test loop (test_step)
+        - Prediction Loop (predict_step)
+        - Optimizers and LR Schedulers (configure_optimizers)
+    Read the docs:
+        https://pytorch-lightning.readthedocs.io/en/latest/common/lightning_module.html
+    """
+
+    def __init__(
+        self,
+        net: torch.nn.Module,
+        optimizer: torch.optim.Optimizer,
+        scheduler: torch.optim.lr_scheduler,
+    ):
+        super().__init__()
+
+        # this line allows to access init params with 'self.hparams' attribute
+        # also ensures init params will be stored in ckpt
+        self.save_hyperparameters(logger=False, ignore=["net"])
+
+        self.net = net
+
+        # loss function
+        self.criterion = torch.nn.CrossEntropyLoss()
+
+        ## metric objects for calculating and averaging accuracy across batches
+        self.train_acc = Accuracy(task="multiclass", num_classes=10)
+        self.val_acc = Accuracy(task="multiclass", num_classes=10)
+        self.test_acc = Accuracy(task="multiclass", num_classes=10)
+
+        # for averaging loss across batches
+        self.train_loss = MeanMetric()
+        self.val_loss = MeanMetric()
+        self.test_loss = MeanMetric()
+
+        # for tracking best so far validation accuracy
+        self.val_acc_best = MaxMetric()
+
+        # for deployment 
+        self.predict_transform = T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  
+        
+
+    def forward(self, x: torch.Tensor):
+        return self.net(x)
+    
+    @torch.jit.export                     # this exports the forward_jit func whenver there is torchscript or trace
+    def forward_jit(self, x: torch.Tensor):
+        # transform the inputs
+        x = self.predict_transform(x)
+        #x = x.unsqueeze(0)
+
+        with torch.no_grad():
+
+            # forward pass
+            out = self(x)
+
+            probabilities = torch.nn.functional.softmax(out[0], dim=0)
+            #confidences = {categories[i]: float(probabilities[i]) for i in range(1000)}
+
+        return probabilities
+
+    def on_train_start(self):
+        # by default lightning executes validation step sanity checks before training starts,
+        # so we need to make sure val_acc_best doesn't store accuracy from these checks
+        self.val_loss.reset()
+        self.val_acc.reset()
+        self.val_acc_best.reset()
+
+    def model_step(self, batch: Any):
+        x, y = batch
+        logits = self.forward(x)
+        loss = self.criterion(logits, y)
+        preds = torch.argmax(logits, dim=1)
+        return loss, preds, y
+
+    def training_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.model_step(batch)
+
+        # log train metrics
+        self.train_loss(loss)
+        self.train_acc(preds, targets)
+        self.log("train/loss", self.train_loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.log("train/acc", self.train_acc, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+
+        # we can return here dict with any tensors
+        # and then read it in some callback or in `training_epoch_end()` below
+        # remember to always return loss from `training_step()` or else backpropagation will fail!
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def on_train_epoch_end(self, outputs: List[Any]):
+        # `outputs` is a list of dicts returned from `training_step()`
+        pass
+
+    def validation_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.model_step(batch)
+
+        # update and log metrics
+        self.val_loss(loss)
+        self.val_acc(preds, targets)
+        self.log("val/loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.log("val/acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        #self.log("hp_metric", loss) #placed in validation epoch end
+
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def on_validation_epoch_end(self):
+        acc = self.val_acc.compute()  # get current val acc
+        self.val_acc_best(acc)  # update best so far val acc
+        # log `val_acc_best` as a value through `.compute()` method, instead of as a metric object
+        # otherwise metric would be reset by lightning after each epoch
+        self.log("val/acc_best", self.val_acc_best.compute(), prog_bar=True, sync_dist=True)
+
+        loss = self.val_loss.compute() #add validation loss to hp_metric
+        self.log("hp_metric", loss, sync_dist=True)
+
+    def test_step(self, batch: Any, batch_idx: int):
+        loss, preds, targets = self.model_step(batch)
+
+        # update and log metrics
+        self.test_loss(loss)
+        self.test_acc(preds, targets)
+        self.log("test/loss", self.test_loss, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("test/acc", self.test_acc, on_step=False, on_epoch=True, prog_bar=True)
+
+        return {"loss": loss, "preds": preds, "targets": targets}
+
+    def test_epoch_end(self, outputs: List[Any]):
+        pass
+
+    def configure_optimizers(self):
+        """Choose what optimizers and learning-rate schedulers to use in your optimization.
+        Normally you'd need one. But in the case of GANs or similar you might have multiple.
+
+        Examples:
+            https://lightning.ai/docs/pytorch/latest/common/lightning_module.html#configure-optimizers
+        """
+        optimizer = self.hparams.optimizer(params=self.parameters())
+        if self.hparams.scheduler is not None:
+            scheduler = self.hparams.scheduler(optimizer=optimizer)
+            return {
+                "optimizer": optimizer,
+                "lr_scheduler": {
+                    "scheduler": scheduler,
+                    "monitor": "val/loss",
+                    "interval": "epoch",
+                    "frequency": 1,
+                },
+            }
+        return {"optimizer": optimizer}
+
+
+if __name__ == "__main__":
+    _ = TIMMLitModule(None, None, None)
